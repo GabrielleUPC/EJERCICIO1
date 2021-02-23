d76877c73c046d6ddc74e4ccee3b01d3e0ddc23c
GabrielleUPC <79283702+GabrielleUPC@users.noreply.github.com>
2021-02-22 23:52:21 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace EstructuraCondicionalSimple2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota1, nota2, nota3;
+            string linea;
+            Console.Write("Ingrese primer nota:");
+            linea = Console.ReadLine();
+            nota1 = int.Parse(linea);
+            Console.Write("Ingrese segunda nota:");
+            linea = Console.ReadLine();
+            nota2 = int.Parse(linea);
+            Console.Write("Ingrese tercer nota:");
+            linea = Console.ReadLine();
+            nota3 = int.Parse(linea);
+            int promedio;
+            promedio = (nota1 + nota2 + nota3) / 3;
+            if (promedio >= 5)
+            {
+                Console.Write("Promocionado");
+            }
+            Console.ReadKey();
+        }
+    }
+}
